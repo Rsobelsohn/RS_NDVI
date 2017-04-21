251f644e66c8e206f4fb1dc267514e2d7fb4c237
Rsobelsohn <rsobelsohn@colgate.edu>
2017-04-21 13:46:48 -0400

    Starting to look at trends and added comments

--- R Updated!!!.R
+++ R Updated!!!.R
@@ -1,28 +1,37 @@
 library(plyr)
 setwd("c:\\Users\\Rob\\Desktop\\NDVI_Research\\net-R\\netR\\NDVI")
+###Read in all the data sets from excel###
+##########################################
 datDC <- read.csv("Dav_Canopy_out.csv")
 datDU <- read.csv("Dav_understory_out.csv")
 datLC <- read.csv("LDF2_canopy_out.csv")
 datLU <- read.csv("LDF2_understory_out.csv")
 
-#combines to colnames specific data set (excluding hour, doy, and year)
+##combines to colnames specific data set (excluding hour, doy, and year)##
+##########################################################################
 colnames(datDC)[1:13] <- paste0(colnames(datDC)[1:13],"DC")
 colnames(datDU)[1:11] <- paste0(colnames(datDU)[1:11],"DU")
 colnames(datLC)[1:9] <- paste0(colnames(datLC)[1:9],"LC")
 colnames(datLU)[1:9] <- paste0(colnames(datLU)[1:9],"LU")
 
-#colnames(datLC) <- paste(colnames(datLC),colnames(datLU))
+##colnames(datLC) <- paste(colnames(datLC),colnames(datLU))##
+##Join the data sets by the three columns####################
 datJoin1 <- join(datDC, datDU, by=c("hour","doy","year"), type="full")
 datJoin2 <- join(datLC, datLU, by=c("hour","doy","year"), type="full")
 datAll <- join(datJoin1, datJoin2, by=c("hour","doy","year"), type="full")
 
-#Calculate new NDVI
+##Calculate new NDVI#####
+##NDVI= (NIR-R)/(NIR+R)##
+#########################
 datAll$ndvi1LC.cor <- (datAll$alpha1DC*datAll$ni800.nm1LC-datAll$ni630.nm1LC)/
 				(datAll$alpha1DC*datAll$ni800.nm1LC+datAll$ni630.nm1LC)
 datAll$ndvi2LC.cor <- (datAll$alpha1DC*datAll$ni80.nm2LC-datAll$ni630.nm2LC)/
 				(datAll$alpha1DC*datAll$ni80.nm2LC+datAll$ni630.nm2LC)
 names(datAll)
 
+##Change the formating of doy##
+##List by year, month, day#####
+###############################
 datDC$date <- as.POSIXlt(paste(datDC$year,datDC$doy,datDC$hour,sep="."),
 				format="%Y.%j.%H")
 datDU$date <- as.POSIXlt(paste(datDU$year,datDU$doy,datDU$hour,sep="."),
@@ -32,18 +41,23 @@ datLC$date <- as.POSIXlt(paste(datLC$year,datLC$doy,datLC$hour,sep="."),
 datLU$date <- as.POSIXlt(paste(datLU$year,datLU$doy,datLU$hour,sep="."),
 				format="%Y.%j.%H")
 Alldat <- merge(datDC,datDU,by="date",all=T)
+
 ##Setting Conditions##
+##Eliminate NDVI less than 0 and greater than 1##
 datAll_NEWDC <- ifelse(datAll$ndviDC<0|datAll$ndviDC>1, NA, datAll$ndviDC)
 datAll_NEWDU <- ifelse(datAll$ndviDU<0|datAll$ndviDU>1, NA, datAll$ndviDU)
 datAll_NEWLC <- ifelse(datAll$ndvi1LC<0|datAll$ndvi1LC>1, NA, datAll$ndvi1LC)
 datAll_NEWLU <- ifelse(datAll$ndvi1LU<0|datAll$ndvi1LU>1, NA, datAll$ndvi1LU)
 
 ##Daily Mean based on doy##
+##Calculate mean per day###
 AveNDVIDC <- aggregate(datAll_NEWDC, by=list(datAll$doy), FUN="mean", na.action=na.omit, na.rm=TRUE)
 AveNDVIDU <- aggregate(datAll_NEWDU, by=list(datAll$doy), FUN="mean", na.action=na.omit, na.rm=TRUE)
 AveNDVILC <- aggregate(datAll_NEWLC, by=list(datAll$doy), FUN="mean", na.action=na.omit, na.rm=TRUE)
 AveNDVILU <- aggregate(datAll_NEWLU, by=list(datAll$doy), FUN="mean", na.action=na.omit, na.rm=TRUE)
 
+##In new data sets rename colums by NDVI and doy##
+##Repeat for four data sets with two columns per##
 colnames(AveNDVIDC) [1] <- "doy"
 colnames(AveNDVIDC) [2] <- "NDVI.dc"
 colnames(AveNDVIDU) [1] <- "doy"
@@ -53,11 +67,14 @@ colnames(AveNDVILC) [2] <- "NDVI.lc"
 colnames(AveNDVILU) [1] <- "doy"
 colnames(AveNDVILU) [2] <- "NDVI.lu"
 
+##Aggregate into one larger data set##
+######################################
 Join1 <- join(AveNDVIDC, AveNDVIDU, by=c("doy"), type="full")
 Join2 <- join(AveNDVILC, AveNDVILU, by=c("doy"), type="full")
 datJoin <- join(Join1, Join2, by=c("doy"), type="full")
 
-
+##Begin to plot the function##
+##############################
 plot(datJoin$doy, datJoin$NDVI.dc, type="l",
      col="black",xlab = "Day of Year", ylab = "NDVI", 
      ylim = c(0,1))
@@ -70,18 +87,33 @@ legend()
 #Blue= NDVI of Dav Understory
 #Red= NDVI of LDF Canopy
 #Green= NDVI of LDF Understory
+
+##Why are green and red higher than blue and black#################
+##At low density stand the NDVI is higher than high density stand##
+##Less plants but healthier at low density#########################
+##Is sattelite data typically lower in low density than high?######
+
 legend(datJoin$doy, datJoin$NDVI),
       c("Dav Canopy","Dav Understory",
         "LDF Canopy", "LDF Understory"),
         col=c("black", "blue", "red", "green")
+#add legend
 
 ##Compare NDVI and LWS##
+##Leaf Wetness Sensor###
 ########################
-datAll.LWS <- ifelse(datAll$LWS.minDU<0|datAll$LWS.minDU>250, NA, datAll$ndviDC)
+datAll.LWS <- ifelse(datAll$LWS.minDU<0|datAll$LWS.minDU>250, NA, datAll$LWS.LWDU)
 AveLWSDU <- aggregate(datAll.LWS, by=list(datAll$doy), FUN="mean", na.action=na.omit, na.rm=TRUE)
 colnames(AveLWSDU) [1] <- "doy"
 colnames(AveLWSDU) [2] <- "LWS"
-##How do I incorporate both NDVI and LWS on a graph)
-#Git testing
+
+##How do I incorporate both NDVI and LWS on a graph)##
+plot(datJoin$doy, datJoin$NDVI.dc, type="l",
+     col="black",xlab = "Day of Year", ylab = "NDVI", 
+        ylim = c(0,1))
+points(AveLWSDU$doy, AveLWSDU$LWS, type="l", col="blue")
+
+        
+
 
 
